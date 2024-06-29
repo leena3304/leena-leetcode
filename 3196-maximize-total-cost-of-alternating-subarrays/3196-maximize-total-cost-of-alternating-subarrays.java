c88cfbaa94104e6408f1d9b5663a7c00713280f1
leena3304 <146821102+leena3304@users.noreply.github.com>
2024-06-29 20:31:38 +0530

    Time: 2 ms (82.96%), Space: 59.4 MB (88.73%) - LeetHub

---
+++ 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long maximumTotalCost(int[] nums) {
+        int n=nums.length;
+        long dp[]=new long[n+1];
+        dp[1]=nums[0];
+        
+        for(int i=1;i<n;i++){
+            long op1=dp[i]+nums[i];
+            long op2=dp[i-1]+nums[i-1]-nums[i];
+            dp[i+1]=Math.max(op1,op2);
+        }
+        return dp[n];
+        
+    }
+}
