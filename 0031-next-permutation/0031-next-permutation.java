18e612eacbcec66a41438c41cb255712a61062b3
leena3304 <146821102+leena3304@users.noreply.github.com>
2024-09-01 19:33:45 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (78.62%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,84 @@
+/*class Solution {
+    public void nextPermutation(int[] nums) {
+        int ind=-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
+            }
+        }
+        if(ind!=-1){
+        for(int i=nums.length-1;i>=ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(i,ind,nums);
+                break;
+            }
+            
+        }
+           reverse(ind+1,nums.length-1,nums);
+          
+        }
+    }
+      
+        
+        private void swap(int a,int b,int nums[]){
+            int temp=nums[a];
+            nums[a]=nums[b];
+            nums[b]=temp;
+        }
+    
+     private void reverse(int start, int end,int nums[]) {
+        while (start < end) {
+            swap(start, end,nums);
+            start++;
+            end--;
+        }
+    }
+}*/
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int ind = -1;
+        
+        // Step 1: Find the first decreasing element from the end
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+        
+        // Step 2: If there was a decreasing element, find the element to swap with
+        if (ind != -1) {
+            for (int i = nums.length - 1; i > ind; i--) {
+                if (nums[i] > nums[ind]) {
+                    swap(nums, i, ind);
+                    break;
+                }
+            }
+        }
+        
+        // Step 3: Reverse the sequence after the index `ind`
+        reverse(nums, ind + 1, nums.length - 1);
+        
+       
+    }
+    
+    // Method to swap two elements in the array
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+    
+    // Method to reverse the array from start to end
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
